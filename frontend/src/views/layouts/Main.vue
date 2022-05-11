f2e9848fce35c51b62133767cb1b83365288e3ea
Daisy <daisy@doomguy.com>
2022-05-11 22:09:16 +0200

    Start adding routing for forward/back for Vue

--- frontend/src/views/layouts/Main.vue
+++ frontend/src/views/layouts/Main.vue
@@ -8,11 +8,22 @@ import { defineComponent } from '@vue/composition-api'
 export default defineComponent({
   mounted () {
     this.goToLogin();
+    window.onpopstate = (e) =>
+    {
+      e.preventDefault();
+      e.stopImmediatePropagation();
+      console.log('stop');
+    };
   },
   methods: {
     goToLogin() {
       this.$router.push('/login');
     },
   },
+  watch: {
+    '$route' (to, from) {
+      console.log(to, from);
+    }
+  }
 })
 </script>

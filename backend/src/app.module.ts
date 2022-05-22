93faea8dd6805ac965fdbe11011a82abe763d653
fdeĉ <fd@42l.fr>
2022-05-22 16:17:09 +0200

    add some little test work

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Match } from './match.entity';
+import { User } from './user.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "user",
+      password: "password",
+      database: "user",
+      entities: [User, Match],
+      synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })

84e7b0fad6bcbcf88bd7520dd1fd635233824bbb
fdeĉ <fd@42l.fr>
2022-06-06 17:56:52 +0200

    adding user and match test files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,21 +2,39 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Match } from './match.entity';
 import { User } from './user.entity';
+import { TestUserModule } from './test_user/test_user.module';
+import { Message } from './message.entity';
+import { Channel } from './channel.entity';
+import { MutedUser } from './mutedUser.entity';
+import { BannedUser } from './bannedUser.entity';
+import { PendingInvitation } from './pendingInvitation.entity';
+import { Match } from './match.entity';
+import { TestMatchModule } from './test_match/test_match.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
       type: "postgres",
       host: "localhost",
       port: 5432,
-      username: "user",
+      username: "transcendence",
       password: "password",
-      database: "user",
-      entities: [User, Match],
+      database: "transcendence",
+      entities: [
+          User,
+          Match,
+          Message,
+          Channel,
+          MutedUser,
+          BannedUser,
+          PendingInvitation,
+        ],
       synchronize: true,
-  })],
-  controllers: [AppController],
-  providers: [AppService],
+  }),
+  TestUserModule,
+  TestMatchModule,
+],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,20 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { TypesOf2FA, User } from './user.entity';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    let repo = User.getRepository();
-    let user1 = repo.create({
-        pseudo: "user1",
-        avatar: "null",
-        password: "pass",
-        typeOf2FA: TypesOf2FA.NONE,
-        valueOf2FA: "null",
-        xp: 0.0,
-    });
-    repo.save(user1);
-    console.log("written new entity!");
     return 'Hello World!';
   }
 }

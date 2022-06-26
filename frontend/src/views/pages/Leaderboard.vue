421c5cecd741cc24934f8e1b63e2702915078beb
fdeĉ <fd@42l.fr>
2022-06-26 21:37:03 +0200

    using table instead of list for leaderboard

--- frontend/src/views/pages/Leaderboard.vue
+++ frontend/src/views/pages/Leaderboard.vue
@@ -1,127 +1,167 @@
 <template>
-  <div class="q-pa-md" style="max-width: 350px">
-    <q-list bordered padding>
-      <q-item>
-        <q-item-section>
-          <q-item-label overline>OVERLINE</q-item-label>
-          <q-item-label>Single line item</q-item-label>
-          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-        </q-item-section>
+	<div class="q-pa-md">
+		<q-table
+			class="my-sticky-virtscroll-table"
+			virtual-scroll
+			v-model:pagination="pagination"
+			:rows-per-page-options="[0]"
+			:virtual-scroll-sticky-size-start="48"
+			row-key="rank"
+			title="Leaderboard"
+			:rows="rows"
+			:columns="columns">
+			<template v-slot:body-cell-picture="props">
+				<q-td :props="props">
+					<img src="image.url"/>
+				</q-td>
+			</template>
+		</q-table>
 
-        <q-item-section side top>
-          <q-item-label caption>5 min ago</q-item-label>
-        </q-item-section>
-      </q-item>
-
-      <q-separator spaced />
-      <q-item-label header>List Header</q-item-label>
-
-      <q-item>
-        <q-item-section avatar>
-          <q-icon color="primary" name="bluetooth" />
-        </q-item-section>
-        <q-item-section>List item</q-item-section>
-        <q-item-section side>
-          <q-item-label caption>meta</q-item-label>
-        </q-item-section>
-      </q-item>
-
-      <q-separator spaced inset="item" />
-
-      <q-item>
-        <q-item-section top avatar>
-          <q-avatar color="primary" text-color="white" icon="bluetooth" />
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label>Single line item</q-item-label>
-          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-        </q-item-section>
-
-        <q-item-section side top>
-          <q-item-label caption>5 min ago</q-item-label>
-          <q-icon name="star" color="yellow" />
-        </q-item-section>
-      </q-item>
-
-      <q-separator spaced inset="item" />
-
-      <q-item>
-        <q-item-section top avatar>
-          <q-avatar color="primary" text-color="white" square icon="bluetooth" />
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label>Single line item</q-item-label>
-          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-        </q-item-section>
-
-        <q-item-section side top>
-          <q-item-label caption>meta</q-item-label>
-        </q-item-section>
-      </q-item>
-
-      <q-separator spaced inset="item" />
-
-      <q-item>
-        <q-item-section top avatar>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-          </q-avatar>
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label>Single line item</q-item-label>
-          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-        </q-item-section>
-
-        <q-item-section side top>
-          <q-badge label="10k" />
-        </q-item-section>
-      </q-item>
-
-      <q-separator spaced inset="item" />
-
-      <q-item>
-        <q-item-section top avatar>
-          <q-avatar rounded>
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-          </q-avatar>
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label>Single line item</q-item-label>
-          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-        </q-item-section>
-
-        <q-item-section side top>
-          <q-item-label caption>meta</q-item-label>
-        </q-item-section>
-      </q-item>
-
-      <q-separator spaced />
-
-      <q-item>
-        <q-item-section top thumbnail class="q-ml-none">
-          <img src="https://cdn.quasar.dev/img/mountains.jpg">
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label>Single line item</q-item-label>
-          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-        </q-item-section>
-
-        <q-item-section side top>
-          <q-item-label caption>meta</q-item-label>
-        </q-item-section>
-      </q-item>
-    </q-list>
-  </div>
+	</div>
 </template>
 
-<script lang="ts">
+<script>
+import { ref } from 'vue';
+const columns = [
+	{
+		name: 'rank',
+		label: 'RANK',
+		field: 'rank',
+		required: true,
+		align: 'left',
+		sortable: true
+	},
+	{
+		name: 'picture',
+		required: true,
+		label: '',
+		align: 'right',
+		field: row => row.picture,
+		sortable: true
+	},
+	{
+		name: 'user',
+		required: true,
+		label: 'PLAYER',
+		align: 'left',
+		field: row => row.user,
+		format: val => `${val}`,
+		sortable: true
+	},
+	{
+		name: 'ratio',
+		label: 'RATIO',
+		field: 'ratio',
+		sortable: true,
+		sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
+	}
+];
+
+const seed = [
+	{
+		rank: '#1',
+		user: 'Frozen Yogurt',
+		picture: 'https://cdn.quasar.dev/img/boy-avatar.png',
+		ratio: '1%'
+	},
+	{
+		rank: '#2',
+		user: 'Ice cream sandwich',
+		picture: 'https://cdn.quasar.dev/img/boy-avatar.png',
+		ratio: '1%'
+	},
+	{
+		rank: '#3',
+		user: 'Eclair',
+		picture: 'https://cdn.quasar.dev/img/boy-avatar.png',
+		ratio: '7%'
+	},
+	{
+		rank: '#4',
+		user: 'Cupcake',
+		picture: 'https://cdn.quasar.dev/img/boy-avatar.png',
+		ratio: '8%'
+	},
+	{
+		rank: '#5',
+		user: 'Gingerbread',
+		picture: 'https://cdn.quasar.dev/img/boy-avatar.png',
+		ratio: '16%'
+	},
+	{
+		rank: '#6',
+		user: 'Jelly bean',
+		picture: 'https://cdn.quasar.dev/img/boy-avatar.png',
+		ratio: '0%'
+	},
+	{
+		rank: '#7',
+		user: 'Lollipop',
+		picture: 'https://cdn.quasar.dev/img/boy-avatar.png',
+		ratio: '2%'
+	},
+	{
+		rank: '#8',
+		user: 'Honeycomb',
+		picture: 'https://cdn.quasar.dev/img/boy-avatar.png',
+		ratio: '45%'
+	},
+	{
+		rank: '#9',
+		user: 'Donut',
+		picture: 'https://cdn.quasar.dev/img/boy-avatar.png',
+		ratio: '22%'
+	},
+	{
+		rank: '#10',
+		user: 'KitKat',
+		picture: 'https://cdn.quasar.dev/img/boy-avatar.png',
+		ratio: '6%'
+	}
+];
+
+// we generate lots of rows here
+let rows = [];
+for (let i = 0; i < 1000; i++)
+	rows = rows.concat(seed.slice(0).map(r => ({ ...r })));
+rows.forEach((row, index) =>
+{
+	row.index = index;
+});
 
 export default {
-	name: 'leaderboardPage'
+	name: 'LeaderboardPage',
+	setup ()
+	{
+		return {
+			columns,
+			rows,
+
+			pagination: ref({
+				rowsPerPage: 50
+			})
+		};
+	}
 };
 </script>
+
+<style lang="sass">
+.my-sticky-virtscroll-table
+  /* height or max-height is important */
+  //height: 410px
+
+  .q-table__top,
+  .q-table__bottom,
+  thead tr:first-child th /* bg color is important for th; just specify one */
+  background-color: #fff
+
+  thead tr th
+  position: sticky
+  z-index: 1
+  /* this will be the loading indicator */
+  thead tr:last-child th
+  /* height of all previous header rows */
+  top: 48px
+  thead tr:first-child th
+  top: 0
+</style>

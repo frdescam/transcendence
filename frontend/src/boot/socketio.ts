889ea04ca464ee12f002457626b5ec36586e43e9
Bastien ADAM <badam@e1r9p4.clusters.42paris.fr>
2022-07-18 23:48:45 +0200

    Import socket's code from cbertran

--- frontend/src/boot/socketio.ts
+++ frontend/src/boot/socketio.ts
@@ -1,5 +1,22 @@
-import { io } from 'socket.io-client';
+import { boot } from 'quasar/wrappers';
+import { Manager, Socket } from 'socket.io-client';
 
-// Using socket.io namespaces: https://socket.io/docs/v4/namespaces/
-export const chatSocket = io('//' + document.location.hostname + ':8080/chat');
-export const gameSocket = io('//' + document.location.hostname + ':8080/game');
+declare module '@vue/runtime-core' {
+	interface ComponentCustomProperties {
+		$socketChat: Socket,
+	}
+}
+
+const manager = new Manager('//' + document.location.hostname + '8080', {
+	autoConnect: true
+});
+const chat = manager.socket('/chat::');
+const game = manager.socket('/game');
+
+export default boot(({ app }) =>
+{
+	app.config.globalProperties.$socketChat = chat;
+	app.provide('socketChat', app.config.globalProperties.$socketChat);
+    app.config.globalProperties.$socketGame = game;
+	app.provide('socketGame', app.config.globalProperties.$socketGame);
+});
--- frontend/src/components/game/game-view.vue
+++ frontend/src/components/game/game-view.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import clsx from 'clsx';
 import { AppFullscreen, Dialog, Notify } from 'quasar';
-import { onBeforeUnmount, onMounted, reactive, readonly, ref, Ref } from 'vue';
-import { gameSocket } from 'src/boot/socketio';
+import { onBeforeUnmount, onMounted, reactive, readonly, ref, Ref, inject} from 'vue';
+import { Socket } from 'socket.io-client';
 import Scene, { mapConfig, options } from './canvas/scene';
 import maps from './maps';
 
@@ -20,6 +20,8 @@ export interface interfaceState {
 	paused: boolean,
 	finish: boolean
 }
+
+const gameSocket: Socket = inject('socketGame') as Socket
 
 const props = defineProps<{ party: string }>();
 

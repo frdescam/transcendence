131de6948209a30335775e843ef1d8fe3373356c
Bastien Adam <badam@student.42.fr>
2022-07-19 19:44:25 +0200

    [TMP] Add user mockup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
   await app.listen(process.env.BACK_PORT);
 }
 bootstrap();
---
+++ backend/src/usermockup/auth.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+
+@Injectable()
+export class SocketMockupAuthGuard implements CanActivate
+{
+    canActivate(context: ExecutionContext): boolean {
+      const request = context.switchToWs().getClient();
+      const user = request.handshake.auth.user;
+      if (!user)
+        return false;
+      const userId = parseInt(user);
+
+      request.user = {id: userId};
+
+      return true;
+    }
+}
+
+@Injectable()
+export class HTTPMockupAuthGuard implements CanActivate
+{
+    canActivate(context: ExecutionContext): boolean {
+      const request = context.switchToHttp().getRequest();
+      const user = request.cookies.USER;
+      console.log(user);
+      if (!user)
+        return false;
+      const userId = parseInt(user);
+
+      request.user = {id: userId};
+
+      return true;
+    }
+}

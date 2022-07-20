926410ae6461f84b0311f3ec67f7bd531a00be32
fdeĉ <fd@42l.fr>
2022-07-20 08:24:37 +0200

    fix space between 2 lists in profile page

--- frontend/src/views/pages/Profile2.vue
+++ frontend/src/views/pages/Profile2.vue
@@ -37,65 +37,69 @@
 			</q-item>
 		</div>
 		<!-- END HEADER -->
-		<div class="row justify-evenly q-pa-md">
+		<div class="row justify-evenly">
 			<!-- MATCHES -->
-			<q-list class="col-12 col-md-6 q-mb-md q-pb-sm shadow-2 rounded-borders">
-				<q-toolbar>
-					<q-toolbar-title>Matches</q-toolbar-title>
-					<div>
-						<q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
-							<template v-slot:append>
-								<q-icon name="search"/>
-							</template>
-						</q-input>
-					</div>
-				</q-toolbar>
-				<q-item v-for="match in matches" :key="match.id">
-					<q-card class="fit q-pa-md" v-bind:style="{ 'background-color': (match.winner == 1) ? 'lightblue' : 'red' }">
-						<p>{{ match.timestamp }}</p>
-						<div class="row justify-center">
-							<q-avatar>
-								<img src='https://cdn.quasar.dev/img/boy-avatar.png'>
-							</q-avatar>
-							<p class="q-px-sm q-my-auto">User1</p>
-							<p class="q-px-sm q-my-auto">{{ match.userHomeScore }}</p>
-							<p class="q-px-sm q-my-auto">-</p>
-							<p class="q-px-sm q-my-auto">{{ match.userForeignScore }}</p>
-							<p class="q-px-sm q-my-auto">User2</p>
-							<q-avatar>
-								<img src='https://cdn.quasar.dev/img/boy-avatar.png'>
-							</q-avatar>
+            <div class="col-12 col-md-6 q-py-md q-pl-md q-pr-sm">
+				<q-list class="q-mb-md q-pb-sm shadow-2 rounded-borders">
+					<q-toolbar>
+						<q-toolbar-title>Matches</q-toolbar-title>
+						<div>
+							<q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+								<template v-slot:append>
+									<q-icon name="search"/>
+								</template>
+							</q-input>
 						</div>
-						<p class="text-center q-mb-none">map : {{ match.map }}</p>
-					</q-card>
-				</q-item>
-			</q-list>
+					</q-toolbar>
+					<q-item v-for="match in matches" :key="match.id">
+						<q-card class="fit q-pa-md" v-bind:style="{ 'background-color': (match.winner == 1) ? 'lightblue' : 'red' }">
+							<p>{{ match.timestamp }}</p>
+							<div class="row justify-center">
+								<q-avatar>
+									<img src='https://cdn.quasar.dev/img/boy-avatar.png'>
+								</q-avatar>
+								<p class="q-px-sm q-my-auto">User1</p>
+								<p class="q-px-sm q-my-auto">{{ match.userHomeScore }}</p>
+								<p class="q-px-sm q-my-auto">-</p>
+								<p class="q-px-sm q-my-auto">{{ match.userForeignScore }}</p>
+								<p class="q-px-sm q-my-auto">User2</p>
+								<q-avatar>
+									<img src='https://cdn.quasar.dev/img/boy-avatar.png'>
+								</q-avatar>
+							</div>
+							<p class="text-center q-mb-none">map : {{ match.map }}</p>
+						</q-card>
+					</q-item>
+				</q-list>
+			</div>
 			<!-- MATCHES END -->
 			<!-- ACHIEVEMENTS -->
-			<q-list class="col-12 col-md-6 q-mb-md q-pb-sm rounded-borders shadow-2">
-				<q-toolbar>
-					<q-toolbar-title>Achievements</q-toolbar-title>
-						<div>
-						<q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
-							<template v-slot:append>
-								<q-icon name="search"/>
-							</template>
-						</q-input>
-					</div>
-				</q-toolbar>
-				<q-item v-for="achievement in achievements" :key="achievement.id">
-					<q-card class="fit row justify-between q-pa-md">
-						<div class="column">
-							<p>{{ achievement.timestamp }}</p>
-							<p class="q-mb-sm">{{ achievement.achievementName }}</p>
-							<q-item-label caption>{{ achievement.achievementDescription }}</q-item-label>
+            <div class="col-12 col-md-6  q-py-md q-pr-md q-pl-sm">
+				<q-list class="q-mb-md q-pb-sm rounded-borders shadow-2">
+					<q-toolbar>
+						<q-toolbar-title>Achievements</q-toolbar-title>
+							<div>
+							<q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+								<template v-slot:append>
+									<q-icon name="search"/>
+								</template>
+							</q-input>
 						</div>
-						<q-avatar size="75px">
-							<img :src='achievement.achievementIcon'>
-						</q-avatar>
-					</q-card>
-				</q-item>
-			</q-list>
+					</q-toolbar>
+					<q-item v-for="achievement in achievements" :key="achievement.id">
+						<q-card class="fit row justify-between q-pa-md">
+							<div class="column">
+								<p>{{ achievement.timestamp }}</p>
+								<p class="q-mb-sm">{{ achievement.achievementName }}</p>
+								<q-item-label caption>{{ achievement.achievementDescription }}</q-item-label>
+							</div>
+							<q-avatar size="75px">
+								<img :src='achievement.achievementIcon'>
+							</q-avatar>
+						</q-card>
+					</q-item>
+				</q-list>
+			</div>
 			<!-- ACHIEVEMENTS END -->
 		</div>
 	</div>

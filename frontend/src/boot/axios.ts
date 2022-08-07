94e42afb0e4c0a4d3ae608f12f23acde096c14ce
fdeĉ <fd@42l.fr>
2022-08-07 23:27:02 +0200

    now really working

--- frontend/src/boot/axios.ts
+++ frontend/src/boot/axios.ts
@@ -13,7 +13,7 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'http://127.0.0.1:8080/api/' });
+const api = axios.create({ baseURL: 'http://127.0.0.1:8080/api/', withCredentials: true });
 
 export default boot(({ app }) =>
 {

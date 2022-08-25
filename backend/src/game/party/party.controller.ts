7054c3a1c0119247a1d7e6ea891738b404df24fb
Bastien ADAM <badam@student.42.fr>
2022-08-25 13:37:10 +0200

    Fix indentation

--- backend/src/game/party/party.controller.ts
+++ backend/src/game/party/party.controller.ts
@@ -11,82 +11,82 @@ export class PartyController
   constructor (private partyService: PartyService)
   {}
     
-    @Get()
+  @Get()
   findAll(): getPartyDto[]
   {
     return (this.partyService.getAllAsJSON());
   }
 
-    @UseGuards(HTTPMockupAuthGuard)
-    @Get('mine')
-    findMine(
-        @Request() req,
-    ): string | null
-    {
-      const user: any = req.user;
-      this.partyService.checkUserObject(user);
-      const himself: userId = user.id;
-      const party = this.partyService.findPartyWithUser(himself);
+  @UseGuards(HTTPMockupAuthGuard)
+  @Get('mine')
+  findMine(
+    @Request() req,
+  ): string | null
+  {
+    const user: any = req.user;
+    this.partyService.checkUserObject(user);
+    const himself: userId = user.id;
+    const party = this.partyService.findPartyWithUser(himself);
 
-      return (party ? party.room : null);        
-    }
+    return (party ? party.room : null);        
+  }
 
-    @UseGuards(HTTPMockupAuthGuard)
-    @Put('giveup')
-    giveup(
-        @Request() req,
-        @Param() params
-    )
-    {
-      const user: any = req.user;
-      this.partyService.checkUserObject(user);
-      const himself: userId = user.id;
-      const party = this.partyService.findPartyWithUser(himself);
-      if (!party)
+  @UseGuards(HTTPMockupAuthGuard)
+  @Put('giveup')
+  giveup(
+    @Request() req,
+    @Param() params
+  )
+  {
+    const user: any = req.user;
+    this.partyService.checkUserObject(user);
+    const himself: userId = user.id;
+    const party = this.partyService.findPartyWithUser(himself);
+    if (!party)
+      return ({left: false});
+    const slot = this.partyService.getSlotFromUser(party, himself);
+    if (slot === -1)
+      throw 'Unexpected state';
+    if (params.room)
+      if (params.room !== party.room)
         return ({left: false});
-      const slot = this.partyService.getSlotFromUser(party, himself);
-      if (slot === -1)
-        throw 'Unexpected state';
-      if (params.room)
-        if (params.room !== party.room)
-          return ({left: false});
 
-      if (this.partyService.admitDefeat(party, slot))
-        return ({left: true});
-      else
-        return ({left: false});
-    }
+    if (this.partyService.admitDefeat(party, slot))
+      return ({left: true});
+    else
+      return ({left: false});
+  }
 
-    @Get(':room')
-    findOne(@Param() params): getPartyDto | null
-    {
-      const partyArr = this.partyService.getAll().filter(({room}) => (room === params.room));
+  @Get(':room')
+  findOne(@Param() params): getPartyDto | null
+  {
+    const partyArr = this.partyService.getAll().filter(({room}) => (room === params.room));
 
-      return (partyArr.length ? this.partyService.partyToPublicJson(partyArr[0]) : null);
-    }
-    
-    @UseGuards(HTTPMockupAuthGuard)
-    @Post()
-    @UsePipes(new ValidationPipe({ transform: true, transformOptions: {enableImplicitConversion: true} }))
-    create(
-        @Body() {room = null, map = 'classic', adversary = null}: CreatePartyDto,
-        @Request() req
-    ): string
-    {
-      const user: any = req.user;
-      this.partyService.checkUserObject(user);
-      const himself: userId = user.id;
+    return (partyArr.length ? this.partyService.partyToPublicJson(partyArr[0]) : null);
+  }
+  
+  @UseGuards(HTTPMockupAuthGuard)
+  @Post()
+  @UsePipes(new ValidationPipe({ transform: true, transformOptions: {enableImplicitConversion: true} }))
+  create(
+    @Body() {room = null, map = 'classic', adversary = null}: CreatePartyDto,
+    @Request() req
+  ): string
+  {
+    const user: any = req.user;
+    this.partyService.checkUserObject(user);
+    const himself: userId = user.id;
 
-      let adversaryId = null;
-      if (adversary && typeof adversary === 'string' && !isNaN(parseInt(adversary)))
-        adversaryId = parseInt(adversary);
-      // @TODO: check adversary ID existence
+    let adversaryId = null;
+    if (adversary && typeof adversary === 'string' && !isNaN(parseInt(adversary)))
+      adversaryId = parseInt(adversary);
+    // @TODO: check adversary ID existence
 
-      const party = this.partyService.createParty(
-        room,
-        map,
-        [himself, adversaryId]
-      );
-      return (party.room);
-    }
+    const party = this.partyService.createParty(
+      room,
+      map,
+      [himself, adversaryId]
+    );
+    return (party.room);
+  }
 }

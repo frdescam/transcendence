19f9448400481174064620ebb8bc98f2ac397700
Paul OHL <paul.lv.ohl@gmail.com>
2022-08-25 13:39:48 +0200

    Imported and slightly revisited Buom's games list

---
+++ frontend/src/views/pages/List.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+export default {
+	name: 'list-page'
+};
+</script>
+
+<script setup lang="ts">
+import List from '../../components/games/list-view.vue';
+</script>
+
+<template>
+	<q-page class="full-width row wrap justify-center">
+		<div class="col-10 col-xl-8 col-lg-10 col-md-12 col-sm-12 col-xs-12">
+			<List/>
+		</div>
+	</q-page>
+	<!-- Go to the top FAB -->
+</template>

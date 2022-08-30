a2ed6fb7b965bc92444f732e4204b2fddfb6d9c1
Bastien ADAM <badam@student.42.fr>
2022-08-30 03:03:41 +0200

    Add centered layout

---
+++ frontend/src/views/layouts/Centered.vue
@@ -0,0 +1,19 @@
+<template>
+	<None>
+		<q-page class="row items-center justify-center" padding>
+      <router-view />
+		</q-page>
+	</None>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import None from './None.vue';
+
+export default defineComponent({
+	name: 'CenteredLayout',
+	components: {
+		None
+	}
+});
+</script>

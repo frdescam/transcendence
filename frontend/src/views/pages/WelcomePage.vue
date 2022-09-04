691b79670136bf30fbd4ef73221bbad660b85a43
c-bertran <clement.bertrand22@gmail.com>
2022-09-05 01:13:01 +0200

    Patch socket inside ProfilePage

--- frontend/src/views/pages/WelcomePage.vue
+++ frontend/src/views/pages/WelcomePage.vue
@@ -88,7 +88,6 @@ export default ({
 		async function fetchUserInfo ()
 		{
 			const res = await api.get('/user/me', {});
-			console.log(res.data);
 			user.value = res.data;
 		}
 		async function fetchFriendList ()

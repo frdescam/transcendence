a0c9827734cffa3d0993f6b53b5759500306b314
Bastien ADAM <badam@student.42.fr>
2022-09-04 18:44:54 +0200

    Remove unused import

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,6 @@ import * as process from 'process';
 import * as cookieParser from 'cookie-parser';
 
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import cors from './cors';
 import { HttpExceptionFilter, NotFoundExceptionFilter } from './filter';

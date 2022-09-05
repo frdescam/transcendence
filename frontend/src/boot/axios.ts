c31370229420ed06bf2bc58dd8acfaddcf2f67dd
Bastien ADAM <badam@student.42.fr>
2022-09-05 21:40:29 +0200

    Provide default catcher for axios

--- frontend/src/boot/axios.ts
+++ frontend/src/boot/axios.ts
@@ -1,5 +1,7 @@
 import { boot } from 'quasar/wrappers';
 import axios, { AxiosInstance } from 'axios';
+import { Notify } from 'quasar';
+import type { AxiosError, AxiosResponse } from 'axios';
 
 declare module '@vue/runtime-core' {
 	interface ComponentCustomProperties {
@@ -15,10 +17,45 @@ export const api = axios.create({
 	withCredentials: true
 });
 
+export type catchAxiosType = ((got: Promise<AxiosResponse<unknown> | void>) => Promise<AxiosResponse<unknown> | void>);
+function catchAxios (got: Promise<AxiosResponse<unknown> | void>): Promise<AxiosResponse<unknown> | void>
+{
+	return (
+		got.catch(
+			(err: AxiosError) =>
+			{
+				Notify.create({
+					progress: true,
+					timeout: 10000,
+					type: 'negative',
+					message: err.message,
+					caption: typeof err.cause !== 'undefined' ? (err.cause + '') : undefined
+				});
+			}
+		)
+	);
+}
+
+export type printAxiosErrorType = ((err: AxiosError) => void);
+function printAxiosError (err: AxiosError): void
+{
+	Notify.create({
+		progress: true,
+		timeout: 10000,
+		type: 'negative',
+		message: err.message,
+		caption: typeof err.cause !== 'undefined' ? (err.cause + '') : undefined
+	});
+}
+
 export default boot(({ app }) =>
 {
 	app.config.globalProperties.$axios = axios;
 	app.config.globalProperties.$api = api;
+	app.config.globalProperties.$catchAxios = catchAxios;
+	app.config.globalProperties.$printAxiosError = printAxiosError;
 	app.provide('axios', app.config.globalProperties.$axios);
 	app.provide('api', app.config.globalProperties.$api);
+	app.provide('catchAxios', app.config.globalProperties.$catchAxios);
+	app.provide('printAxiosError', app.config.globalProperties.$printAxiosError);
 });
